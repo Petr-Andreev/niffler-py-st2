28afdd252a20b875ff8dbaefdbff1a2eae8a153b
Petr Andreev <drakula2805@gmail.com>
2025-02-25 13:27:24 +0300

    Обновил тесты, добавил удаление категории через БД

--- niffler-e-2-e-tests-python/marks.py
+++ niffler-e-2-e-tests-python/marks.py
@@ -10,4 +10,10 @@ class TestData:
     category = lambda x: pytest.mark.parametrize('category', [x], indirect=True)
     spends = lambda x: pytest.mark.parametrize('spends', [x],
                                                indirect=True,
-                                               ids=lambda param: param['description'])
+                                               ids=lambda param: param)
+    spends_list = lambda *spend_data_list: pytest.mark.parametrize(
+        'spends_list',  # Имя фикстуры
+        [list(spend_data_list)],  # Список с JSON-объектами
+        indirect=True,
+        ids=lambda params: f"spends_{len(params)}"  # Генерация ID для теста
+    )
